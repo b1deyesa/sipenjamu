3f5e7ed438ad520b2ab7b3c5eaa0ffe0394ffcfe
= <=>
2025-09-24 21:35:12 +0700

    Auto Commit

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/sass/app.scss'],
+            input: ['resources/sass/app.scss', 'resources/js/app.js'],
             refresh: true,
         }),
         tailwindcss(),
